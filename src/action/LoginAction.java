c17b6806e6abd925191551704662799723b18dac
rilac <eodos6480@gmail.com>
2023-04-03 16:49:15 +0900

    LoginAction 수정중

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -12,29 +12,13 @@ public class LoginAction implements Action{
     public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
         ActionForward forward = new ActionForward();
         InsightDAO dao = new InsightDAO();
-
-        HashMap<String, String> = new HashMap<>();
-        UserDTO user = new UserDTO();
         String userid = request.getParameter("userid");
-        String username = request.getParameter("username");
         String userpw = request.getParameter("userpw");
-        String useremail = request.getParameter("useremail");
-        String useraddr = request.getParameter("useraddr");
-        String userphone = request.getParameter("userphone");
 
-        user.setUserid(userid);
-        user.setUsername(username);
-        user.setUserpw(userpw);
-        user.setUseremail(useremail);
-        user.setUseraddr(useraddr);
-        user.setUserphone(userphone);
+        if (dao.login(userid, userpw)) {
 
-        if(dao.register(user)){
-            forward.setPath("/mainhome.in");
-        }else{
-            forward.setPath("/mainhome.in");
         }
-        forward.setRedirect(true);
+
         return forward;
     }
 }
--- src/dao/InsightDAO.java
+++ src/dao/InsightDAO.java
@@ -5,6 +5,8 @@ import mybatis.SqlMapConfig;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import java.util.HashMap;
+
 public class InsightDAO {
     SqlSessionFactory factory = SqlMapConfig.getFactory();
     SqlSession sqlSession;
@@ -18,6 +20,17 @@ public class InsightDAO {
 
         if(sqlSession.insert("Insight.insert", dto) == 1){
             result = true;
+        }
+        return result;
+    }
+
+    public boolean login(String userid, String userpw) {
+        boolean result = false;
+        HashMap<String, String> data = new HashMap<>();
+        data.put("userid", userid);
+        data.put("userpw", userpw);
+        if(sqlSession.selectOne("Insight.LoginCheck", data)){
+
         }
         return result;
     }
